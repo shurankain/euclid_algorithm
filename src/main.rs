e75f292933acf722178ea9e1dda16061002bf31f
Oleksandr Husiev <ohusiev@icloud.com>
2024-06-04 22:25:04 +0200

    basic implementation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = std::env::args().collect();
+
+    let a = args[1].parse::<i64>().unwrap();
+    let b = args[2].parse::<i64>().unwrap();
+
+    println!("Result: {}", euqlid(a, b));
 }
+
+fn euqlid(mut a: i64, mut b: i64) -> i64 {
+    let mut res: i64 = 0;
+    while (b != 0) {
+        res = a % b;
+        a = b;
+        b = res;
+    }
+    return a;
+}
+
+
+
